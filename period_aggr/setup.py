e5bfece473206bf397bdbc1847a5475d3bc29f3d
ravikchikkatest <111889896+ravikchikkatest@users.noreply.github.com>
2022-08-23 15:59:50 +0100

    Add files via upload

---
+++ period_aggr/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='period_aggr',
+      version='0.0.1',
+      description='testcode',
+      author='test company ltd',
+      author_email='test@test.com',
+      packages=['job'],
+      zip_safe=False)
